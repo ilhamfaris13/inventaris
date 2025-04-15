17d33955ad7d3520df3212632f95809e1e9ffa1e
Muhammad Ilham Faris <nabilafaustina24@gmail.com>
2025-04-15 10:04:32 +0700

    first commit

---
+++ resources/views/divisi/index.blade.php
@@ -0,0 +1,1 @@
+<h1>ini halaman divisi</h1>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/divisi', function () {
+    return view('divisi.index');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
